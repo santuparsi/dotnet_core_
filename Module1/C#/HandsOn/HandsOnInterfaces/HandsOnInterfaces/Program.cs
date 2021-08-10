965a44569da704febd9b868e0da5a4d3f28b336a
santuparsi <santuparsi@gmail.com>
2021-08-10 12:15:49 +0530

    commit_interfaces

--- Module1/C#/HandsOn/HandsOnInterfaces/HandsOnInterfaces/Program.cs
+++ Module1/C#/HandsOn/HandsOnInterfaces/HandsOnInterfaces/Program.cs
@@ -2,11 +2,48 @@
 
 namespace HandsOnInterfaces
 {
+    interface IShape
+    {
+        void Area(); //abstract method
+    }
+    class Circle : IShape
+    {
+        double r;
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+        public void Area()
+        {
+            Console.WriteLine("Area of Circle: " + Math.PI * r * r);
+        }
+    }
+    class Rectangle : IShape
+    {
+        int l;
+        int w;
+
+        public Rectangle(int l, int w)
+        {
+            this.l = l;
+            this.w = w;
+        }
+
+        public void Area()
+        {
+            Console.WriteLine("Area of Rectangle: " + (l * w));
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IShape circle = new Circle(12.3);
+            circle.Area();
+            IShape rectangle = new Rectangle(12, 34);
+            rectangle.Area();
+            Circle circle1 = new Circle(34.5);
+            circle.Area();
         }
     }
 }
