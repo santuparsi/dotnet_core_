9bc86172d349245ae254bfa94107d0f6ed7c4a07
santuparsi <santuparsi@gmail.com>
2021-08-10 11:07:44 +0530

    commit

--- Module1/C#/HandsOn/HandsOnClassTypes/HandsOnClassTypes/Demo3.cs
+++ Module1/C#/HandsOn/HandsOnClassTypes/HandsOnClassTypes/Demo3.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace HandsOnClassTypes
 {
+    partial class Sample
+    {
+        public void M1() { }
+    }
+    partial class Sample
+    {
+        public void M2() { }
+        public void M3() { }
+    }
+    partial class Sample
+
+    {
+        public void M4() { }
+        public void M5() { }
+    }
     class Demo3
     {
+        static void Main()
+        {
+            Sample obj = new Sample();
+            obj.M1();
+            obj.M3();
+            obj.M5();
+        }
     }
 }
---
+++ Module1/C#/HandsOn/HandsOnInterfaces/HandsOnInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandsOnInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
