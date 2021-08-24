b329eab069fe057515486e1cde4d7d0c055d6eee
santuparsi <santuparsi@gmail.com>
2021-08-24 10:38:50 +0530

    new_commit_Ado

---
+++ Moduel2/Ado.net/HandsOn/HandsOnAdo-Demo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace HandsOnAdo_Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = null;
+           //Read Product table data
+           try
+            {
+                connection = new SqlConnection(@"Data Source=SANTU\MSSQLSERVER2019;Initial Catalog=PracticeDB;Integrated Security=True");
+                connection.Open();
+                SqlCommand command = new SqlCommand("Select * from Product", connection);
+                SqlDataReader reader=command.ExecuteReader();
+                if(reader.HasRows)
+                {
+                   while(reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader[0]} Name: {reader[1]} Price: {reader["Price"]} Stock: {reader["Stock"]}");
+                    }
+                }
+                else
+
+                {
+                    Console.WriteLine("Reader is Empty..");
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close(); //close connection
+            }
+        }
+    }
+}
