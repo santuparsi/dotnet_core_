4c80b58deaf577cf86fb5d2291e98670ea42569f
santuparsi <santuparsi@gmail.com>
2021-09-24 11:43:51 +0530

    new_commit_sample_project

---
+++ Module3/AirLineReservation/AirLineReservation.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AirLineReservation.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
